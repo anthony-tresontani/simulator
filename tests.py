33121fb590fc697f37426604b1b76a0e4ab0cfc1
Anthony TRESONTANI <dev.tresontani@google.com>
2012-01-07 12:21:07 +0100

    add test stubs

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 import unittest
 
 from core.goodproducer import GoodProducer
-from core.goodproducer import NoLabourToPerformAction, IllegalStateToPerformAction, NoInputToBeTransformed
+from core.goodproducer import NoLabourToPerformAction, IllegalStateToPerformAction, NoInputToBeTransformed, InvalidInputLoaded
 
 from core.input import Input
 from core.labour import Labour
@@ -10,9 +10,10 @@ from core.labour import Labour
 class GoodProducerTest(unittest.TestCase):
 
     def setUp(self):
-    	self.gp = GoodProducer()
+	config = {"input_types":["yarn"]}
+    	self.gp = GoodProducer(config)
         self.labour = Labour()
-        self.inputs = Input()
+        self.inputs = Input("yarn")
 
     def test_state_idle(self):
 	self.assertEquals(self.gp.get_state(), GoodProducer.IDLE)
@@ -57,7 +58,8 @@ class GoodProducerTest(unittest.TestCase):
 	self.assertEquals(len(self.gp.get_outputs()), 1) 
 
     def test_slower_machine(self):
-	config = {'rate_by_minute': 0.5}
+	config = {'rate_by_minute': 0.5,
+		  "input_types":["yarn"],}
 	slower_gp = GoodProducer(config)
 
         slower_gp.affect(self.labour)
@@ -66,3 +68,24 @@ class GoodProducerTest(unittest.TestCase):
         slower_gp.start()
         slower_gp.produce(2)    
         self.assertEquals(len(slower_gp.get_outputs()), 1)
+
+    def test_invalid_input(self):
+	input = Input("rocks")
+	
+	self.assertRaises(InvalidInputLoaded, self.gp.load, input)
+
+    
+    def test_out_of_stock(self):
+	self.assertTrue(False)
+
+
+    def test_multiple_inputs(self):
+	self.assertTrue(False)
+
+    
+    def test_complete_failure(self):
+	 self.assertTrue(False)
+
+    def test_partial_failure(self):
+        # slow down the machine
+	self.assertTrue(False)
