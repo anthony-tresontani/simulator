51d150aa53ded823761e9c9392605ed101e4e664
anthony-tresontani <tresontani@gmail.com>
2012-02-12 23:23:19 +0000

    Minor refactoring for configuration management

--- configuration/__init__.py
+++ configuration/__init__.py
@@ -5,18 +5,29 @@ from core.production_unit import ProductionUnit
 from core.specification import Specification, MaterialInputConstraint
 from core.worker import Worker
 
+def create_materials(yaml):
+    materials = {}
+    for material in yaml.get("materials", []):
+        materials[material["type"]] = {"type": material["type"], "price": material["price"]}
+    return materials
+
+
+def create_spec(materials, production_unit):
+    spec = Specification()
+    for input in production_unit.get("inputs", []):
+        spec.add(MaterialInputConstraint(Material(type=input["input_type"], quantity=input["input_quantity"])))
+    for output in production_unit.get("outputs", []):
+        spec.add_output_material(Material(type=output["input_type"], quantity=output["input_quantity"],
+                                          price=materials[output["input_type"]]["price"]))
+    return spec
+
+
 def get_factory(yaml_conf):
     yaml = load(yaml_conf)
     factory = Factory(name=yaml["name"])
-    materials = {}
-    for material in yaml.get("materials",[]):
-        materials[material["type"]] = {"type":material["type"], "price":material["price"]}
+    materials = create_materials(yaml)
     for production_unit in yaml["production_units"]:
-        spec = Specification()
-        for input in production_unit.get("inputs", []):
-            spec.add(MaterialInputConstraint(Material(type= input["input_type"], quantity= input["input_quantity"])))
-        for output in production_unit.get("outputs", []):
-            spec.add_output_material(Material(type= output["input_type"], quantity= output["input_quantity"], price=materials[output["input_type"]]["price"]))
+        spec = create_spec(materials, production_unit)
         config = {}
         config["rate_by_minute"]= production_unit.get("rate", 1)
         factory.add_production_unit(ProductionUnit(spec=spec, config=config,  name=production_unit["name"]))
